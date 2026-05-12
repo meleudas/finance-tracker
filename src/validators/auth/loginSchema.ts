d73b509ab837e5ab827050910a2b3e1b59f068ea
Ki-mik0 <dienslaif@gmail.com>
2026-05-12 19:10:40 +0300

    feat(auth): add validation schemas

---
+++ src/validators/auth/loginSchema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Email is required" })
+    .check(z.email({ error: "Invalid email address" }))
+    .regex(/^[^@]*@(?!.*\.ru$).*$/, { message: "Russian domains are not allowed" })
+    .max(255, { message: "Email is too long" }),
+  password: z.string().min(1, { message: "Password is required" }),
+});
---
+++ src/validators/auth/registerSchema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Email is required" })
+    .check(z.email({ error: "Invalid email address" }))
+    .regex(/^[^@]*@(?!.*\.ru$).*$/, { message: "Russian domains are not allowed" })
+    .max(255, { message: "Email is too long" }),
+
+  password: z
+    .string()
+    .min(8, { message: "Must be at least 8 characters" })
+    .regex(/[A-Z]/, { message: "Must contain at least one uppercase letter" })
+    .regex(/\d/, { message: "Must contain at least one number" })
+    .regex(/[!@#$%^&*(),.?":{}|<>]/, { message: "Must contain at least one special character" }),
+});
